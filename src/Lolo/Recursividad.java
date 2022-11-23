a73fe2b54f04b3d213cffed8684614a9062fb999
Albert <beto@beto.cat>
2022-11-23 20:21:55 +0100

    Recursividad, palindromo texto

--- src/Lolo/Recursividad.java
+++ src/Lolo/Recursividad.java
@@ -2,17 +2,29 @@ package Lolo;
 
 public class Recursividad {
 
-    public static int sumaSuc(int n){
-        int suma=0;
+    /*public static int sumaSuc(int n){
         if(n==0){
         return 0;
         }else {
                 return n+sumaSuc(n-1);
         }
-
     }
-
     public static void main(String[] args) {
         System.out.println(sumaSuc(4));
+    }*/
+
+    public static boolean paliTexto(String j){
+        boolean palindromo= true;
+        if(j.length()==0 || j.length()==1){
+return true;}
+else if(j.charAt(0)==j.charAt(j.length()-1)){
+    return paliTexto(j.substring(1,j.length()-1));
+}else{
+    return false;
+        }
     }
+
+
+
+    //class
 }
